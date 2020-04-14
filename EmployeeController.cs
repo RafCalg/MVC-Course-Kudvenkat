7271d103d447b3c33941031581cbf5ff5f7fac53
RafCalg <rafikb66@yahoo.com>
2020-04-14 08:38:14 -0600

    Add files via upload

---
+++ EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            EmployeeContext employeeContext = new EmployeeContext();//Establish a connection to the database - Uses EmployeeContext.cs and Web.config connectionString="server=localhost\sqlexpress; database=Sample; integrated security=SSPI"
+
+            List<Employee> employees = employeeContext.Employees.ToList();
+
+            return View(employees);
+        }
+
+        public ActionResult Details(int id)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+
+            Employee employee = employeeContext.Employees.Single(emp => emp.EmployeeId == id);
+
+            return View(employee);
+        }
+    }
+    
+}
