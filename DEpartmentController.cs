80e01a8cf8c5a2b5b368d60caa201daf8b3b7ea2
RafCalg <rafikb66@yahoo.com>
2020-04-14 08:42:30 -0600

    Add files via upload

---
+++ DEpartmentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: DEpartment
+        public ActionResult Index()
+        {
+            EmployeeContext employeeContext = new EmployeeContext(); // Connection to the databse
+            List<Department> departments = employeeContext.Department.ToList();
+
+            return View(departments);
+
+        }
+    }
+}
--- EmployeeController.cs
+++ EmployeeController.cs
@@ -10,11 +10,11 @@ namespace WebApplication1.Controllers
     public class EmployeeController : Controller
     {
 
-        public ActionResult Index()
+        public ActionResult Index(int departmentId)
         {
             EmployeeContext employeeContext = new EmployeeContext();//Establish a connection to the database - Uses EmployeeContext.cs and Web.config connectionString="server=localhost\sqlexpress; database=Sample; integrated security=SSPI"
 
-            List<Employee> employees = employeeContext.Employees.ToList();
+            List<Employee> employees = employeeContext.Employees.Where(employee => employee.DepartmentId == departmentId).ToList();
 
             return View(employees);
         }
